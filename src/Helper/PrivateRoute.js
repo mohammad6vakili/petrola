974c99e4cbd571f70f8b733dd40489cac7ec09d5
mohammad6vakili <59134745+mohammad6vakili@users.noreply.github.com>
2021-11-30 20:59:35 +0330

    ui bugs fixed

--- src/Helper/PrivateRoute.js
+++ src/Helper/PrivateRoute.js
@@ -14,7 +14,7 @@ const PrivateRoute = (props) => {
           profile ? (
           <Component {...props} />
         ) : (
-          history.replace("/")
+          history.replace("/login")
         )
       }
     />
